07035446256858eace17e183d33bd42aa371ef28
Aaron Siegel <siegelaaron94@gmail.com>
2016-04-04 16:11:16 -0700

    added AOA USB skeleton.

--- src/main.c
+++ src/main.c
@@ -2,8 +2,8 @@
 #pragma config DSWDTPS = DSWDTPSF // DSWDT Postscale Select (1:2,147,483,648 (25.7 days))
 #pragma config DSWDTOSC = LPRC // Deep Sleep Watchdog Timer Oscillator Select (DSWDT uses Low Power RC Oscillator (LPRC))
 #pragma config RTCOSC = SOSC // RTCC Reference Oscillator Select (RTCC uses Secondary Oscillator (SOSC))
-#pragma config DSBOREN = OFF // Deep Sleep BOR Enable bit (BOR disabled in Deep Sleep)
-#pragma config DSWDTEN = OFF // Deep Sleep Watchdog Timer (DSWDT disabled)
+#pragma config DSBOREN = ON // Deep Sleep BOR Enable bit (BOR enabled in Deep Sleep)
+#pragma config DSWDTEN = ON // Deep Sleep Watchdog Timer (DSWDT enabled)
 
 // CONFIG3
 #pragma config WPFP = WPFP63 // Write Protection Flash Page Segment Boundary (Highest Page (same as page 21))
@@ -16,13 +16,13 @@
 // CONFIG2
 #pragma config POSCMOD = HS // Primary Oscillator Select (HS Oscillator mode selected)
 #pragma config I2C1SEL = PRI // I2C1 Pin Select bit (Use default SCL1/SDA1 pins for I2C1 )
-#pragma config IOL1WAY = OFF // IOLOCK One-Way Set Enable (Once set, the IOLOCK bit cannot be cleared)
-#pragma config OSCIOFNC = OFF // OSCO Pin Configuration (OSCO pin functions as clock output (CLKO))
+#pragma config IOL1WAY = ON // IOLOCK One-Way Set Enable (Once set, the IOLOCK bit cannot be cleared)
+#pragma config OSCIOFNC = ON // OSCO Pin Configuration (OSCO pin functions as port I/O (RA3))
 #pragma config FCKSM = CSDCMD // Clock Switching and Fail-Safe Clock Monitor (Sw Disabled, Mon Disabled)
-#pragma config FNOSC = FRCDIV // Initial Oscillator Select (Fast RC Oscillator with Postscaler (FRCDIV))
-#pragma config PLL96MHZ = OFF // 96MHz PLL Startup Select (96 MHz PLL Startup is enabled automatically on start-up)
-#pragma config PLLDIV = NODIV // USB 96 MHz PLL Prescaler Select (Oscillator input used directly (4 MHz input))
-#pragma config IESO = ON // Internal External Switchover (IESO mode (Two-Speed Start-up) enabled)
+#pragma config FNOSC = PRIPLL // Initial Oscillator Select (Primary Oscillator with PLL module (XTPLL, HSPLL, ECPLL))
+#pragma config PLL96MHZ = ON // 96MHz PLL Startup Select (96 MHz PLL Startup is enabled automatically on start-up)
+#pragma config PLLDIV = DIV3 // USB 96 MHz PLL Prescaler Select (Oscillator input divided by 3 (12 MHz input))
+#pragma config IESO = OFF // Internal External Switchover (IESO mode (Two-Speed Start-up) disabled)
 
 // CONFIG1
 #pragma config WDTPS = PS32768 // Watchdog Timer Postscaler (1:32,768)
@@ -35,7 +35,36 @@
 #pragma config JTAGEN = OFF // JTAG Port Enable (JTAG port is disabled)
 
 #include <xc.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <ev_master.h>
+
+#include <usb.h>
+#include <usb_host_android.h>
+
+#define MAX_ALLOWED_CURRENT 500 //mA
+
+static char manufacturer[] = "Personal Transportation Solutions";
+static char model[] = "EvMaster";
+static char description[] = "LIN master node";
+static char version[] = "0.0.1";
+static char uri[] = "N/A";
+static char serial[] = "N/A";
+
+ANDROID_ACCESSORY_INFORMATION device_info = {
+    manufacturer,
+    sizeof(manufacturer),
+    model,
+    sizeof(model),
+    description,
+    sizeof(description),
+    version,
+    sizeof(version),
+    uri,
+    sizeof(uri),
+    serial,
+    sizeof(serial)
+};
 
 static void master_task_5ms()
 {
@@ -57,6 +86,10 @@ int main()
     struct l_irqmask irqmask = { 6, 6 };
     l_sys_irq_restore(irqmask);
 
+    // Setup the AOA and USB
+    AndroidAppStart(&device_info);
+    USBHostInit(0);
+
     // Setup a 5ms timer
     T1CONbits.TON = 1;
     T1CONbits.TSIDL = 0;
@@ -71,9 +104,11 @@ int main()
     IPC0bits.T1IP = 7;
 
     // Use the default schedule table defined in ev_master.ncf
-    l_sch_set_UART1(default, 0);
+    l_sch_set_UART1(normal, 0);
 
     while (true) {
+        USBHostTasks();
+        AndroidTasks();
     }
     return -1;
 }
@@ -97,6 +132,44 @@ void l_sys_irq_restore(struct l_irqmask previous)
     IPC3bits.U1TXIP = previous.tx_level;
     IFS0bits.U1RXIF = 0;
     IEC0bits.U1TXIE = 1;
+}
+
+bool usb_application_event_handler(uint8_t address, USB_EVENT event, void* data, uint32_t size)
+{
+    switch ((int)event) {
+    case EVENT_VBUS_REQUEST_POWER: {
+        if (((USB_VBUS_POWER_EVENT_DATA*)data)->current <= (MAX_ALLOWED_CURRENT / 2))
+            return true;
+        break;
+    }
+
+    case EVENT_VBUS_RELEASE_POWER:
+    case EVENT_HUB_ATTACH:
+    case EVENT_UNSUPPORTED_DEVICE:
+    case EVENT_CANNOT_ENUMERATE:
+    case EVENT_CLIENT_INIT_ERROR:
+    case EVENT_OUT_OF_MEMORY:
+    case EVENT_UNSPECIFIED_ERROR:
+    case EVENT_DETACH:
+    case EVENT_ANDROID_DETACH: {
+        // TODO save the handle
+        return true;
+    }
+    case EVENT_ANDROID_ATTACH: {
+        // TODO release the handle
+        return true;
+        break;
+    }
+    default: {
+        break;
+    }
+    }
+    return false;
+}
+
+bool usb_application_data_event_handler(uint8_t address, USB_EVENT event, void* data, uint32_t size)
+{
+    return false;
 }
 
 void __attribute__((interrupt, no_auto_psv)) _T1Interrupt()
@@ -129,4 +202,9 @@ void __attribute__((interrupt, no_auto_psv)) _U1RXInterrupt()
         if (U1STAbits.FERR)
             U1STAbits.FERR = 0;
     }
+}
+
+void __attribute__((interrupt, auto_psv)) _USB1Interrupt()
+{
+    USB_HostInterruptHandler();
 }
